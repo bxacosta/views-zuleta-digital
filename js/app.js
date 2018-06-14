37107c4576dc6743c34f11665d2e01bcff77c33f
Bryan Acosta <bryan-ax@hotmail.com>
2018-06-14 10:12:46 -0500

    Added dashboard page

--- js/app.js
+++ js/app.js
@@ -52,14 +52,7 @@ $(".counter").each(function() {
 /**
  * Preloader
  */
-/* $(function() {
-    $('img').each(function() {
-        $(this).load(function() {
-            $("#preloader").fadeOut("slow");
-        });
-    });
-}); */
-$(window).on('load', function() {
+$(window).on("load", function() {
     $("#preloader").fadeOut("slow");
 });
 
@@ -70,3 +63,73 @@ $(document).on("click", '[data-toggle="lightbox"]', function(event) {
     event.preventDefault();
     $(this).ekkoLightbox();
 });
+
+/**
+ * Dashboard
+ */
+(function($) {
+    "use strict"; // Start of use strict
+    // Configure tooltips for collapsed side navigation
+    $('.navbar-sidenav [data-toggle="tooltip"]').tooltip({
+        template:
+            '<div class="tooltip navbar-sidenav-tooltip" role="tooltip" style="pointer-events: none;"><div class="arrow"></div><div class="tooltip-inner"></div></div>'
+    });
+    // Toggle the side navigation
+    $("#sidenavToggler").click(function(e) {
+        e.preventDefault();
+        $("body").toggleClass("sidenav-toggled");
+        $(".navbar-sidenav .nav-link-collapse").addClass("collapsed");
+        $(
+            ".navbar-sidenav .sidenav-second-level, .navbar-sidenav .sidenav-third-level"
+        ).removeClass("show");
+        
+        if ($("body").hasClass("sidenav-toggled")) {
+            $(".sidenav-submenu").hide();
+            $(".toggle-icon").removeClass("fa-angle-left");
+            $(".toggle-icon").addClass("fa-angle-right");
+        } else {
+            $(".sidenav-submenu").show();
+            $(".toggle-icon").removeClass("fa-angle-right");
+            $(".toggle-icon").addClass("fa-angle-left");
+        }
+    });
+    // Force the toggled class to be removed when a collapsible nav link is clicked
+    $(".navbar-sidenav .nav-link-collapse").click(function(e) {
+        e.preventDefault();
+        $(".sidenav-submenu").show();
+    });
+    // Prevent the content wrapper from scrolling when the fixed side navigation hovered over
+    $(
+        "body.fixed-nav .navbar-sidenav, body.fixed-nav .sidenav-toggler, body.fixed-nav .navbar-collapse"
+    ).on("mousewheel DOMMouseScroll", function(e) {
+        var e0 = e.originalEvent,
+            delta = e0.wheelDelta || -e0.detail;
+        this.scrollTop += (delta < 0 ? 1 : -1) * 30;
+        e.preventDefault();
+    });
+    // Scroll to top button appear
+    $(document).scroll(function() {
+        var scrollDistance = $(this).scrollTop();
+        if (scrollDistance > 100) {
+            $(".scroll-to-top").fadeIn();
+        } else {
+            $(".scroll-to-top").fadeOut();
+        }
+    });
+    // Configure tooltips globally
+    $('[data-toggle="tooltip"]').tooltip();
+    // Smooth scrolling using jQuery easing
+    $(document).on("click", "a.scroll-to-top", function(event) {
+        var $anchor = $(this);
+        $("html, body")
+            .stop()
+            .animate(
+                {
+                    scrollTop: $($anchor.attr("href")).offset().top
+                },
+                1000,
+                "easeInOutExpo"
+            );
+        event.preventDefault();
+    });
+})(jQuery); // End of use strict
